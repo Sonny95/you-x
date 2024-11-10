e8e328210cf811fcbf67a41b1426992f6fdcafdb
Sonny95 <thswlgml9598@gmail.com>
2024-11-11 01:02:18 +1100

    input field

--- src/app/page.js
+++ src/app/page.js
@@ -12,27 +12,27 @@ export default function Home() {
       { id: 1, title: "Discover your perfect loan" },
       { id: 2, label: "Home" },
       { id: 3, label: "Car" },
-      { id: 2, label: "Equipment" },
-      { id: 3, label: "Personal" },
-      { id: 2, label: "Business" },
-      { id: 3, label: "Insurance" },
+      { id: 4, label: "Equipment" },
+      { id: 5, label: "Personal" },
+      { id: 6, label: "Business" },
+      { id: 7, label: "Insurance" },
     ],
-    1: [
+    2: [
       { id: 1, title: "why are you applying?" },
       { id: 2, label: "I want to refinance my loan" },
       { id: 3, label: "I want to purchase a new property" },
     ],
-    2: [
+    3: [
       { id: 1, title: "Have you found your property?" },
       { id: 2, label: "No, I'm still on the hunt" },
       { id: 3, label: "Yes, i've found a property" },
     ],
-    3: [
+    4: [
       { id: 1, title: "This loan is for" },
       { id: 2, label: "It's Just me " },
       { id: 3, label: "There's two of us" },
     ],
-    4: [
+    5: [
       { id: 1, title: "I'm looking for" },
       { id: 2, label: "A home to live in " },
       { id: 3, label: "An investment property" },
@@ -44,43 +44,47 @@ export default function Home() {
 
   const handleButtonClick = (id) => {
     setSelectedOption(id);
-    if (step < 3) {
+    if (step < 6) {
       setStep(step + 1);
     }
   };
 
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-black">
-      <div className="w-1/4 h-3/4">
-        <div className="text-2xl w-full h-1/6 flex items-center justify-between">
-          <div className="flex-grow text-center mr-8 text-[#f39d8e] font-bold">
-            Discover your perfect loan{" "}
-          </div>
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-1/3 h-2/3 bg-white p-4">
+        {/* Title Section */}
+        <div className="w-full flex justify-center items-center mb-4">
+          {options[step].map((option) =>
+            option.title ? (
+              <div key={option.id} className="text-2xl text-center text-[#f39d8e] font-bold">
+                {option.title}
+              </div>
+            ) : null
+          )}
         </div>
-        <div className="w-full flex flex-wrap justify-between">
-          {loans.map((name, index) => (
-            <button
-              key={index}
-              className="cursor:point m-2 w-36 h-20 rounded-xl text-base bg-[#f39d8e] mb-4 hover:bg-yellow-500"
-            >
-              {name}
-            </button>
-          ))}
-        </div>
-        <div>
-          <h1>버튼 선택</h1>
-          <h2>현재 단계: {step}</h2>
 
-          <div>
-            {options[step].map((option) => (
-              <div key={options.id} onClick={() => handleButtonClick(option.id)}>{option.title}</div>
-              <button key={option.id} onClick={() => handleButtonClick(option.id)}>
+        {/* Button Section */}
+        <div className="w-full flex flex-wrap justify-start">
+          {options[step].map((option) =>
+            option.label ? (
+              <button
+                key={option.id}
+                className="font-bold text-xl cursor-pointer m-5 w-2/5 h-20 rounded-xl bg-[#f39d8e] mb-4 hover:bg-yellow-500"
+                onClick={() => handleButtonClick(option.id)}
+              >
                 {option.label}
               </button>
-            ))}
-          </div>
+            ) : null
+          )}
+        </div>
 
-          {selectedOption && <div>선택된 옵션 ID: {selectedOption}</div>}
+        <div className="w-full flex justify-center items-center mb-4">
+          <div className="text-2xl text-center text-[#f39d8e] font-bold">
+            Expecting Property Price
+          </div>
+          <input placeholder="600,000" className="w-"></input>
+          <div className="text-2xl text-center text-[#f39d8e] font-bold">Your deposit price</div>
+          <input placeholder="60,000" className="w-"></input>
         </div>
       </div>
     </div>
